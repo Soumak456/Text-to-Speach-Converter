04f6f0fc0ec7c129980bb5843318767433e74424
Soumak Mitra <142566845+Soumak456@users.noreply.github.com>
2023-08-24 12:19:13 +0530

    Minor Changes

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-let speach = new SpeechSynthesisUtterance();
-
+let speech = new SpeechSynthesisUtterance();
 let voices = [];
-
 let voiceSelect = document.querySelector("select");
 
 window.speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
-    speach.voice = voices[0];
+    speech.voice = voices[0];
 
     voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
 };
 
-voiceSelect.addEventListener("change", () =>{
-    speach.voice = voices[voiceSelect.value]
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
 });
 
+document.querySelector("#listenButton").addEventListener("click", () => {
+    const text = document.querySelector("textarea").value;
+    speech.text = text;
+    window.speechSynthesis.speak(speech);
 
+    // After the speech synthesis, generate a downloadable audio file
+    speech.onend = () => {
+        const audioBlob = new Blob([new Uint8Array(speech.audioBuffer)], { type: "audio/wav" });
+        const audioUrl = URL.createObjectURL(audioBlob);
 
-document.querySelector("button").addEventListener("click", () =>{
-    speach.text = document.querySelector("textarea").value;
-    window.speechSynthesis.speak(speach);
+        const a = document.createElement("a");
+        a.href = audioUrl;
+        a.download = "speech.wav";
+        a.style.display = "none";
+        document.body.appendChild(a);
+
+        a.click();
+
+        document.body.removeChild(a);
+        URL.revokeObjectURL(audioUrl);
+    };
 });
